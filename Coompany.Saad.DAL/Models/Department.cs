689262117a7d64da567c7bdf2c9ce15dca665922
Saad Usama <su656669@gmail.com>
2026-01-19 15:15:13 +0200

    Implemention of DAL

---
+++ Coompany.Saad.DAL/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coompany.Saad.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string code { get; set; }
+
+        public DateTime CreateAt { get; set; }
+    }
+}
